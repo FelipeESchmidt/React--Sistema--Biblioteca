ea7acc2bcd37221bbbd9be2f64f7748f5de8e213
Felipe Schmidt <schmidtfe2@gmail.com>
2021-09-13 09:47:55 -0300

    Criação das Rotas

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '../Header/Header';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Header></Header> 
+      <Switch>
+        <Route exact path="/">
+          <h1>Reading</h1>
+        </Route>
+        <Route path="/reading">
+          <h1>Reading</h1>
+        </Route>
+        <Route path="/want_read">
+          <h1>Want to read</h1>
+        </Route>
+        <Route path="/read">
+          <h1>Read</h1>
+        </Route>
+        <Route path="/">
+          <h1>404</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
